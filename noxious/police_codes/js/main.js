36c32426da66f88e18efa431196144265af2080d
IVelthoven <ivovelthoven@hotmail.com>
2018-12-15 19:18:29 +0100

    Added Police Code page

---
+++ noxious/police_codes/js/main.js
@@ -0,0 +1,21 @@
+
+$(document).ready(function () {
+    let td = $('td');
+
+    // let list = [0, 1, 4, 5, 8, 9, 12, 13, 16, 17, 20, 21, 24, 25, 28, 29];
+
+    let wait = 2;
+    for (i=0;i<td.length;i++) {
+        console.log(i);
+        if (i%2 == 0) {
+            if (wait === 2) {
+                td[i].style.backgroundColor = 'rgba(100,100,100,.8)'; 
+                td[i+1].style.backgroundColor = 'rgba(100,100,100,.8)'; 
+                wait = 0;
+            } else {
+                console.log('WRONG')
+                wait += 2;
+            }
+        }
+    }
+});
